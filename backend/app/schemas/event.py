e76857358ec3e327bbe8e3a0b27a2fc559113cb6
bkaczor <kaczorb97@gmail.com>
2025-05-22 10:12:14 +0200

    bugfixed events api

--- backend/app/schemas/event.py
+++ backend/app/schemas/event.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List, Optional
 from pydantic import BaseModel
 from pydantic.types import StringConstraints
 from datetime import datetime
@@ -8,4 +8,17 @@ class EventCreateModel(BaseModel):
         str,
         StringConstraints(min_length=1),
     ]
-    end_date: datetime
+    type: Annotated[
+        str,
+        StringConstraints(pattern=r'^(once|weekly)$'),
+    ]
+    timezone: Annotated[
+        str,
+        StringConstraints(min_length=1),
+    ]
+    access: Annotated[
+        str,
+        StringConstraints(pattern=r'^(public|restricted)$'),
+    ]
+    end_date: Optional[datetime] = None
+    invitees: Optional[List[str]] = []
